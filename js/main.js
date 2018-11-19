4f38c4530aebbef2f426803427b711054793c51a
Jack Serrino <Jserrino@gmail.com>
2018-11-19 17:30:22 -0500

    Add AJAX email submissions

--- js/main.js
+++ js/main.js
@@ -15,4 +15,51 @@ $(function() {
     var rotation = $(this).scrollTop() / $("#info").offset().top * 135;
     $("#logo").rotate(rotation);
   });
+
+
+  $("#joinform").submit(function(e) {
+    e.preventDefault();
+
+    var value = $("#email").val();
+    if (value == '') {
+      return;
+    }
+
+    $("#joinform > div").removeClass("error");
+    $("#joinform #email").attr('disabled', true);
+    $("#joinform #join").attr('disabled', true);
+    $("#joinform #join").addClass('loading');
+
+    var finished = function() {
+      $("#joinform #email").attr('disabled', false);
+      $("#joinform #join").attr('disabled', false);
+      $("#joinform #join").removeClass('loading');
+      $("#joinform #join").removeClass('blue');
+      $("#joinform #join").removeClass('red');
+    }
+
+    var success = function() {
+      finished();
+      $("#joinform #join").addClass('green');
+      $("#joinform #join").text('Joined!');
+    }
+
+    var fail = function() {
+      finished();
+      $("#joinform #join").addClass('red');
+      $("#joinform > div").addClass("error");
+      $("#joinform #join").text('Error');
+    }
+
+    $.ajax({
+      dataType: 'jsonp',
+      url: "https://getsimpleform.com/messages/ajax?form_api_token=cb27d55a6d9fbaad4231ee66c046135f",
+      data: {
+        email: value,
+      },
+      success: success,
+      error: fail
+    });
+  })
+
 });
